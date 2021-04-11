7aa23845067f5ddf30ee68ab481712f1b0ac410c
Karla Alvarez <karla.alvarezf@gmail.com>
2021-04-11 15:08:53 +0200

    test products

--- functions/retrieve-products.js
+++ functions/retrieve-products.js
@@ -2,6 +2,9 @@ exports.handler = async function(event, context) {
     console.log("Retrieving products")
     return {
         statusCode: 200,
-        body: JSON.stringify({message: "Hello World!"})
+        body: JSON.stringify({products : [
+            {id : "AA001", name: "pizza", price: 13.50},
+            {id : "AA002", name: "coca", price: 3.50}
+        ]})
     };
 } 
