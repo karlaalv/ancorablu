1af8e522437b191205d55f7c26666740f724899d
Karla Alvarez <karla.alvarezf@gmail.com>
2021-04-11 15:17:28 +0200

    test html 3

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
-console.log('funziono')
-document.
-fetch('/.netlify/functions/retrieve-products')
-  .then(response => response.json())
-  .then(data => {
-      console.log(data);
-      document.getElementById('products').innerHTML('Nome: ' + data.products[0].name)
-  });
+document.addEventListener("DOMContentLoaded", function () {
+    console.log('funziono')
+    fetch('/.netlify/functions/retrieve-products')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            document.getElementById('products').innerHTML('Nome: ' + data.products[0].name)
+        });
 });
