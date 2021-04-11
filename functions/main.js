eb7062019d0edd768883749df2393b78546a1698
Karla Alvarez <karla.alvarezf@gmail.com>
2021-04-11 17:04:11 +0200

    removed test that crushed netlify

--- functions/main.js
+++
@@ -1,7 +0,0 @@
-const Commerce = require('@chec/commerce.js');
-const commerce = new Commerce(
-    'pk_test_2592218f2ae77c7a987b880db97b4a8f3843d6a15647a'
-);
-commerce.products.list().then((product) => {
-    console.log("Products retrieved: ", product)
-});
