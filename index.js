58a2ec75671c653e627b74a887877c4c85d8d563
Karla Alvarez <karla.alvarezf@gmail.com>
2021-04-11 15:15:16 +0200

    test html

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+$(function() {
 console.log('funziono')
+document.
 fetch('/.netlify/functions/retrieve-products')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+      console.log(data);
+      document.getElementById('products').innerHTML('Nome: ' + data.products[0].name)
+  });
+});
